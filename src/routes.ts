cd566afe72eac2f2e2159698915c584a70acffc6
Sreeraj <rajan.sreeraj@yahoo.com>
2019-05-12 00:42:53 +0530

    Change examples to ab

--- src/routes.ts
+++ src/routes.ts
@@ -2,5 +2,5 @@ import { Application } from 'express';
 import exampleRoutes from './examples/router';
 
 export default function routes(app: Application): void {
-  app.use('/api/v1/examples/', exampleRoutes);
+  app.use('/api/v1/ab/', exampleRoutes);
 }
